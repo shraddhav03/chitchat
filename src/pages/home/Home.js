199e4818dc56f1aa3cb38a701c570cac91d3bc82
shraddhav03 <97801495+shraddhav03@users.noreply.github.com>
2023-07-28 15:12:50 +0200

    signin and signup page

---
+++ src/pages/home/Home.js
@@ -0,0 +1,14 @@
+import './Home.css'
+
+export const Home = () =>{
+    return(
+        <>
+        <div className="All-heading-div">
+            Home
+        </div>
+        <div className="home-parent-div">
+
+        </div>
+        </>
+    )
+}
